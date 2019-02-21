37c6cc116903d3886514f20cf51978a2c3abc680
Frederico Martins Porto <martinsporto@gmail.com>
2019-02-21 10:35:06 -0300

    Add index js and configure morgan and body parser middlewares

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const { PORT, LOG_MODE } = process.env;
+
+console.log('==', PORT, LOG_MODE);
+
+const app = express();
+
+app.use(morgan(LOG_MODE));
+app.use(bodyParser.json({ type: 'application/*+json' }));
+
+app.listen(PORT || 3000);
